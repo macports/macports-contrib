d002fd7e565a1fc3e3427c069f11b40462e0eb2e
Joe Rozner <jrozner@macports.org>
2010-05-27 19:44:29 +0000

    commit of initial new rails project
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc10-mpwa@68140 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '9ee09b463058c9fe5952ecf75a77b262ace6a79e85b70e885402d3b2acfbf7b65b5a1f4c70a3da3d04ca55a0fa2e069d79836252d587a593fbb49c8d2209ca31';
