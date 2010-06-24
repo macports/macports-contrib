49136d4dea96a185d2d68973c84e37cf94ab31da
Joe Rozner <jrozner@macports.org>
2010-06-24 23:46:40 +0000

    removed undeeded routes
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc10-mpwa@69140 d073be05-634f-4543-b044-5fe20cf6d1d6

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :ports do |port|
+  map.resources :ports, :except => [:edit, :update] do |port|
     port.resources :comments, :except => [:index, :show, :new]
   end
 
