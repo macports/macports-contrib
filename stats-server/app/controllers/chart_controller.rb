f5152a462ae7a658376aa05fd50b1445199c5f07
Derek Ingrouville <derek@macports.org>
2011-08-09 02:57:27 +0000

    Added ChartController class which provides methods to manage charts. Controllers that have views which display charts will inherit from this class.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc11-statistics@82154 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ stats-server/app/controllers/chart_controller.rb
@@ -0,0 +1,54 @@
+# This is a base class for controllers that display charts
+# It contains helpful methods for managing those charts
+
+class ChartController < ApplicationController
+
+  # Associate chart name with a data set
+  def add_chart(chart_name, dataset, populate_code)
+    set_chart_attribute chart_name, :data, dataset
+    set_chart_attribute chart_name, :populate, populate_code
+  end
+  
+  # Set the chart's title
+  def set_chart_title(chart, title)
+    set_chart_attribute chart, :title, title
+  end
+  
+  # Set the chart's type
+  def set_chart_type(chart, type)
+    set_chart_attribute chart, :type, type
+  end
+    
+  # Call a chart's populate_code to populate it with its associated dataset
+  def populate_chart(chart_name, chart)
+    populate = @charts[chart_name][:populate]
+    populate.call(chart_name, chart)
+  end
+  
+  def chart_dataset(chart_name)
+    @charts[chart_name][:data]
+  end
+  
+  def chart_title(chart_name)
+    @charts[chart_name][:title]
+  end
+  
+  def chart_type(chart_name)
+    @charts[chart_name][:type]
+  end
+  
+  private 
+  
+  # Associate an attribute with the given chart
+  def set_chart_attribute(chart, attribute, value)
+    attrs = @charts[chart]
+    
+    if attrs.nil?
+      attrs = Hash.new
+    end
+    
+    attrs[attribute] = value
+    @charts[chart] = attrs
+  end
+  
+end
