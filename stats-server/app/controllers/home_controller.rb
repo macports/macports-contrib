39e52fa34927b33d82e09fd4e74ee4f2a247c234
Derek Ingrouville <derek@macports.org>
2011-08-21 23:05:41 +0000

    Home page now shows:
    - Total number of participating users
    - Number of participating users last month
    - Line chart of number of participating users each month for the past 12 months
    
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc11-statistics@82902 d073be05-634f-4543-b044-5fe20cf6d1d6

--- stats-server/app/controllers/home_controller.rb
+++ stats-server/app/controllers/home_controller.rb
@@ -1,6 +1,55 @@
-class HomeController < ApplicationController
+class HomeController < ChartController
   
-  def index
+  # Populate the users chart
+  def populate_users(chart_name, chart)
+    chart.string "Month"
+    chart.number "Number of users"
+
+    dataset = chart_dataset chart_name
+    
+    dataset.each do |item, count|
+      chart.add_row([item, count])
+    end
   end
+  
+  # Gather number of participating users over past 12 months
+  def gather_users_over_time
+    monthly_users = Hash.new(0)
+    
+    now = Time.now
+    now_d = now.to_date
+    
+    # Iterate over months from 11 months ago to 0 months ago (current month)
+    11.downto(0) do |i|
+      month = now.months_ago(i).to_date
+      
+      
+      # Get the number of active (submitting) users for this month
+      entries = InstalledPort.where(:created_at => (month.at_beginning_of_month)..(month.at_end_of_month))
+      count = entries.select("DISTINCT(user_id)").count
+    
+      # Key is the abbreviated month name and the year (eg: Aug 2011)
+      key = month.to_time.strftime("%b %Y")
+      monthly_users[key] = count
+      
+      # Get users last month
+      if i == 1
+        @users_last_month = count
+        @last_month = month.to_time.strftime("%B")
+      end
+    end
+    
+    add_chart :participating_users, monthly_users, method(:populate_users)
+  end
+    
+  def index
+    @charts = Hash.new
+    gather_users_over_time
+    
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+  
 
 end
