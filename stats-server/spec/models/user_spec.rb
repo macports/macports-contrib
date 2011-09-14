f3d823ce9f90681577d598adf394d481ce5f1039
Derek Ingrouville <derek@macports.org>
2011-09-14 01:36:51 +0000

    Added spec dir for rspec specifications. Added models/user_spec.rb to test the User model
    - Verify the presence of ActiveRecord associations
    - Verify that User has a uuid present
    - Verify that User has a valid uuid
    
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc11-statistics@83971 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ stats-server/spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe User do
+  before(:each) do
+    @user = User.new
+  end
+  
+  it "should have one os_statistic" do
+    @user.should have_one :os_statistic
+  end
+  
+  it "should have many ports" do
+    @user.should have_many :installed_ports
+  end
+  
+  it "should have a uuid present" do
+    @user.should be_invalid
+    @user.uuid = 'BD5C379F-F485-4C89-A3DB-20A3476116F1'
+    @user.should be_valid
+  end
+  
+  it "should only have a valid uuid" do
+    @user.uuid = 'ABC-INVALID-UUID'
+    @user.should be_invalid
+    @user.errors.on(:uuid).should == "uuid must be a valid universally unique identifier"
+            
+    # A valid UUID - generated with uuidgen
+    @user.uuid = 'BD5C379F-F485-4C89-A3DB-20A3476116F1'
+    @user.should be_valid
+  end
+end
