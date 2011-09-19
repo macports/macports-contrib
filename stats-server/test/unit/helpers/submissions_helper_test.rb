b58f85b537d98181acce7d3850b6c28650d97533
Derek Ingrouville <derek@macports.org>
2011-09-19 19:23:52 +0000

    Removed test directory - testing is done with rspec (see spec/ directory)
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc11-statistics@84161 d073be05-634f-4543-b044-5fe20cf6d1d6

--- stats-server/test/unit/helpers/submissions_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SubmissionsHelperTest < ActionView::TestCase
-end
--- stats-server/test/unit/installed_port_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class InstalledPortTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
