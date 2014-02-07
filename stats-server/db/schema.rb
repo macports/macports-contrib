50ed8419f62ecc0f7389441d3a958722ea37e5b3
Clemens Lang <cal@macports.org>
2014-02-07 19:32:44 +0000

    macports stats: committing changes to schema.rb that have been sitting in my installation for ages now; I don't remember how they got there.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc11-statistics@116792 d073be05-634f-4543-b044-5fe20cf6d1d6

--- stats-server/db/schema.rb
+++ stats-server/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,16 +15,16 @@ ActiveRecord::Schema.define(:version => 20110718213618) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "installed_ports", :force => true do |t|
     t.integer  "port_id"
     t.string   "version"
     t.text     "variants"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.integer  "user_id"
   end
 
@@ -31,8 +32,8 @@ ActiveRecord::Schema.define(:version => 20110718213618) do
   add_index "installed_ports", ["user_id"], :name => "index_installed_ports_on_user_id"
 
   create_table "os_statistics", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
     t.string   "macports_version"
     t.string   "osx_version"
     t.string   "os_arch"
@@ -56,22 +57,22 @@ ActiveRecord::Schema.define(:version => 20110718213618) do
     t.string   "maintainers"
     t.string   "platforms"
     t.string   "categories"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   add_index "ports", ["name"], :name => "index_ports_on_name"
 
   create_table "submissions", :force => true do |t|
     t.string   "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
     t.string   "uuid"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
