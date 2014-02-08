825e0566430fd3a69721b23cd0e56c14be419ccd
Clemens Lang <cal@macports.org>
2014-02-08 16:17:48 +0000

    macports stats: add FAQ and Credits
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc11-statistics@116851 d073be05-634f-4543-b044-5fe20cf6d1d6

--- stats-server/config/routes.rb
+++ stats-server/config/routes.rb
@@ -1,17 +1,13 @@
 StatsServer::Application.routes.draw do
-  
+
   get "home/index"
 
   resources :submissions
-
   resources :os_statistics
-  
   resources :installed_ports
-  
   resources :categories, :only => [:index] do
     match '/ports/page/:page', :to => 'ports#index', :page => :page
-    resources :ports, :only => [:index, :show] 
-      
+    resources :ports, :only => [:index, :show]
   end
 
   match '/ports/page/:page', :to => 'ports#index', :page => :page
@@ -19,7 +15,7 @@ StatsServer::Application.routes.draw do
   match '/ports/search', :to => 'ports#search_generate', :as => :search_generate
   match '/ports/search/:criteria/:val/page/:page', :to => 'ports#search', :criteria => :criteria, :val => :val, :page => :page
   match '/ports/search/:criteria/:val', :to => 'ports#search', :criteria => :criteria, :val => :val, :as => :search
-  
+
   root :to => 'home#index'
-  
+  match ':action' => 'static#:action'
 end
